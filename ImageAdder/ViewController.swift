3cff22fcc0ad11aae88353ab8c3b75a509de1585
Vladislav Green <vladzel@gmail.com>
2022-12-07 16:53:13 +0300

    Initial Commit

---
+++ ImageAdder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageAdder
+//
+//  Created by Vladislav Green on 12/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
